e69c4ffc9d7eec4eb101e49c41113f7ca2f01fc6
Bezumec <BezTH@ya.ru>
2021-09-29 06:54:58 +0300

    jsStart

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+var name = "Bez";
+
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+
+let leftBorderWidth = 200;
+
+// number
+// string - "" , '' , ``
+// true/false
+// null
+// undefined
+// let onj = {
+// name: 'apple',
+// color: 'green',
+// weight: '200'
+// }
+// Symbol
+// alert(1);
+// console.log(number);
+// let answer = confirm('Hello World!');
+// console.log(answer);
+
+// let answer = prompt('Hello World!', '');
+// console.log(answer);
+
+// console.log(4+4);
+
+let isChecked = true,
+    isClose = false;
+
+// console.log(isChecked && isClose);
+
+console.log(isChecked || isClose);
