a8d1f6fc57b91d346ac08fb4a116360e497e4bc7
Bezumec <BezTH@ya.ru>
2021-10-04 06:48:35 +0300

    mass

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,52 @@
-var name = "Bez";
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 300,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrowL.png"</button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrowR.png"</button>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    dots: false,
+                    arrows: false
+                }
+            }
+        ]
+        });
 
-let number = 7;
-const pi = 3.14;
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.catalog__con').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+    
+    // $('.catalog-item__link').each(function(i) {
+    //     $(this).on('click', function(e) {
+    //         e.preventDefault();
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //     });
+    // });
 
-number = 4;
+    // $('.catalog-item__link2').each(function(i) {
+    //     $(this).on('click', function(e) {
+    //         e.preventDefault();
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //     });
+    // });
 
-let leftBorderWidth = 200;
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
 
-// number
-// string - "" , '' , ``
-// true/false
-// null
-// undefined
-// let onj = {
-// name: 'apple',
-// color: 'green',
-// weight: '200'
-// }
-// Symbol
-// alert(1);
-// console.log(number);
-// let answer = confirm('Hello World!');
-// console.log(answer);
-
-// let answer = prompt('Hello World!', '');
-// console.log(answer);
-
-// console.log(4+4);
-
-let isChecked = true,
-    isClose = false;
-
-// console.log(isChecked && isClose);
-
-console.log(isChecked || isClose);
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__link2');
+  });
