62f73b4e72949af6f18c8f07f0b816a0d145af28
Bezumec <BezTH@ya.ru>
2021-10-05 05:31:26 +0300

    jsjsjsjs

--- src/js/script.js
+++ src/js/script.js
@@ -49,4 +49,37 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__link2');
-  });
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_submit').on('click', function() {
+        $('#order').fadeOut('slow');
+        $('.overlay, #thanks').fadeIn('slow');
+    });
+        
+    $('.button_mini').each(function(i) { 
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+        
+    $(window).on('click', function(e) {
+        if (e.target.classList.contains('overlay')) {
+            $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+        }
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode === 27) {   // esc
+           $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+        }
+      });
+});
